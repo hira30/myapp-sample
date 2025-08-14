37f6fedc6cfe4fdb43dacb92dbb3aad1477a2112
hirahira <th11282@gmail.com>
2025-08-15 08:36:22 +0900

    プロジェクト ファイルを追加します。

---
+++ MyApp.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+namespace MyApp.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_ReturnsCorrectDifference()
+    {
+        var calc = new Calculator();
+        Assert.Equal(1, calc.Subtract(3, 2));
+    }
+}
